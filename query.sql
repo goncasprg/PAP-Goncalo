3e18af1be4265602d2b7f7f7d1f5de4993a3ab44
Gonçalo <luxpipper1432@gmail.com>
2025-02-01 21:30:19 +0000

    so adicionei o resto do codigo da base de dados

--- query.sql
+++ query.sql
@@ -20,12 +20,12 @@ CREATE TABLE cars (
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY, -- Identificador único do utilizador
-    first_name VARCHAR(50) NOT NULL,  -- Nome
-    last_name VARCHAR(50) NOT NULL,   -- Sobrenome
-    email VARCHAR(100) NOT NULL UNIQUE, -- Email (deve ser único)
-    password VARCHAR(255) NOT NULL,   -- Palavra-passe (hash, por questões de segurança)
-    phone VARCHAR(15),                -- Telefone
-    role ENUM('Admin', 'Client') DEFAULT 'Cliente', -- Papel do utilizador no sistema
+    first_name VARCHAR(50) NOT NULL,   -- Nome
+    last_name VARCHAR(50) NOT NULL,    -- Sobrenome
+    email VARCHAR(100) NOT NULL UNIQUE,-- Email
+    password VARCHAR(255) NOT NULL,    -- Palavra-passe (hash, por questões de segurança)
+    phone VARCHAR(15),                 -- Telefone
+    role ENUM('admin', 'client') DEFAULT 'client', -- Papel do utilizador no sistema
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data de criação do registo
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Última atualização
 );
@@ -58,7 +58,25 @@ CREATE TABLE car_images (
     image_url VARCHAR(255) NOT NULL,
     FOREIGN KEY (car_id) REFERENCES cars(id)
 );
+
 CREATE TABLE car_categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE
 );
+
+INSERT INTO cars (brand, model, registration_year, mileage, seats, fuel_type, power, engine_capacity, transmission, color, warranty, price, description)
+VALUES
+('Audi', 'A3 1.6 TDI Sport', 2018, 120000, 5, 'Diesel', 110, 1.6, 'Manual', 'Preto', 12, 10600.00, 'Carro desportivo compacto, excelente para cidade e estrada.'),
+('BMW', 'Série 3 2.0d M Sport', 2020, 90000, 5, 'Diesel', 190, 2.0, 'Automática', 'Azul', 24, 15800.00, 'Sedan premium com alto desempenho e conforto.'),
+('Mercedes', 'Classe C 2.2 CDI Elegance', 2017, 110000, 5, 'Diesel', 170, 2.2, 'Automática', 'Cinza', 18, 20300.00, 'Luxuoso e confortável, ideal para viagens longas.'),
+('Nissan', 'Qashqai 1.5 dCi Tekna', 2021, 30000, 5, 'Diesel', 115, 1.5, 'Manual', 'Branco', 36, 38500.00, 'SUV moderno com tecnologia avançada e baixo consumo.'),
+('Volkswagen', 'Golf 1.4 TSI Highline', 2019, 80000, 5, 'Gasolina', 150, 1.4, 'Automática', 'Vermelho', 24, 18900.00, 'Compacto esportivo com excelente desempenho e economia.');
+
+INSERT INTO car_images (car_id, image_url)
+VALUES
+(1, 'assets/images/carros/Audi_A3.jpg'),
+(2, 'assets/images/carros/BMW_Serie3.jpg'),
+(3, 'assets/images/carros/Mercedes_ClasseC.jpg'),
+(4, 'assets/images/carros/Nissan_Qashqai.jpg'),
+(5, 'assets/images/carros/VW_Golf.jpg');
+

3ab3ba494aa26d1a916e02264fb76e6d433dcb99
Goncasprg <luxpipper1432@gmail.com>
2025-07-11 09:07:01 +0100

    header fix

--- assets/html/about_us.php
+++ assets/html/about_us.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,34 +11,42 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="../../assets/css/header.css">
 </head>
+
 <body>
     <!-- Header -->
     <?php include("../html/header.php"); ?>
 
     <main class="main-content">
         <section class="about-section inicio">
-            <img src="../images/marcas/banner.png" alt="Imagem de banner representando o início da CarChoice">
+            <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?auto=format&fit=crop&w=900&q=80"
+                alt="Começo">
             <div class="text-content left">
                 <h2>Como tudo começou</h2>
-                <p>CarChoice é uma plataforma inovadora que nasceu da paixão por automóveis e da necessidade de simplificar o processo de compra de veículos. Desde o início, nosso objetivo tem sido conectar compradores e vendedores de uma forma mais eficiente e transparente.</p>
+                <p>CarChoice é uma plataforma inovadora que nasceu da paixão por automóveis e da necessidade de
+                    simplificar o processo de compra de veículos. Desde o início, o nosso objetivo tem sido conectar
+                    compradores e vendedores de uma forma mais eficiente e transparente.</p>
             </div>
         </section>
 
         <section class="about-section missao">
-            <img src="../images/carros/Audi_A3.jpg" alt="Imagem de um Audi A3 simbolizando a missão da CarChoice">
             <div class="text-content right">
                 <h2>A nossa Missão</h2>
-                <p>Facilitar a experiência de compra e venda de carros, oferecendo uma plataforma intuitiva, segura e repleta de recursos úteis.</p>
+                <p>Facilitar a experiência de compra e venda de carros, oferecendo uma plataforma intuitiva, segura e
+                    repleta de recursos úteis.</p>
             </div>
+            <img src="https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?auto=format&fit=crop&w=900&q=80"
+                alt="Missão">
         </section>
 
         <section class="about-section valores">
-            <img src="../images/marcas/banner3.webp" alt="Imagem de banner representando os valores da CarChoice">
+            <img src="https://images.unsplash.com/photo-1490902931801-d6f80ca94fe4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Valores">
             <div class="text-content left">
                 <h2>Os nossos Valores</h2>
                 <ul>
                     <li>Transparência: Acreditamos que a honestidade é fundamental em todas as interações.</li>
-                    <li>Inovação: Estamos sempre em busca de novas soluções para melhorar a experiência do utilizador.</li>
+                    <li>Inovação: Estamos sempre em busca de novas soluções para melhorar a experiência do utilizador.
+                    </li>
                     <li>Compromisso: Trabalhamos arduamente para garantir a satisfação dos nossos clientes.</li>
                 </ul>
             </div>
@@ -47,4 +56,5 @@
     <!-- Footer -->
     <?php include("../html/footer.php"); ?>
 </body>
+
 </html>
